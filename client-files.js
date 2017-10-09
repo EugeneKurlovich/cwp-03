29c94fceb1ea9f0e11686f06c10c3004b93a1877
Eugene Kurlovich <zhenikpggkurlovich@gmail.com>
2017-10-10 01:54:08 +0300

    create dir for clients

--- client-files.js
+++ client-files.js
@@ -1,16 +1,29 @@
 const net = require('net');
 const port = 8124;
-
+const fs = require('fs');
+const path = require('path');
 const startConnect = 'FILES';
 const serverOK = 'ACK';
 const serverNO = 'DEC';
-
+let allFiles = [];
+const nextFileStatus = 'NEXTFILE';
 const client = new net.Socket();
 
 client.setEncoding('utf8');
 
 client.connect(port, function() 
 {
+    console.log("Input Dir: ");
+
+        for (let i = 0; i < process.argv.length- 2; i ++)
+        {
+        console.log("Path: " + process.argv[i+2]);
+        }
+    
+     getDirectory().forEach((i) => {
+        readFiles(i);
+    });
+
         client.write(startConnect);   
 });
 
@@ -19,16 +32,14 @@ client.on('data', function(data) {
     if (data === serverOK) 
     {
         console.log("Connected is open");
-        console.log("Input Dir: ");
 
-        for (let i = 0; i < process.argv.length- 2; i ++)
-        {
-        console.log("Path: " + process.argv[i+2]);
-        }
 
-        client.destroy();
 
+
+         client.destroy();
     }
+    
+
     else if (data === serverNO) 
     {
         console.log(data);
@@ -40,3 +51,23 @@ client.on('data', function(data) {
 
 
 });
+
+
+function readFiles(file) {
+    fs.readdirSync(file).forEach((i) => {
+
+        let filePath = path.normalize(file + '\\' + i);
+        if (fs.statSync(filePath).isFile()) {
+            allFiles.push(filePath);
+        }
+        else {
+            readAllFilesNames(filePath);
+        }
+    })
+}
+
+
+function getDirectory() 
+{
+    return process.argv.slice(2);
+}
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const net = require('net');
+const fs = require('fs');
 const port = 8124;
-
+const path = require('path');
 const serverOK = 'ACK';
 const serverNO = 'DEC';
 const startConnect = 'FILES';
 let counter = 0;
+let Clients = [];
+let filesClientsValue = 0; 
+let filesChanks = [];
+
+const recvFilesDirpath = process.env.FILESDIR +  'files';
 
 
 const server = net.createServer((client) => 
@@ -13,18 +19,26 @@ const server = net.createServer((client) =>
 
 client.on('data',ConnectUsers);
 
+
+
+
  function ConnectUsers(data, err) 
     {
-        if (!err)
-         {
-            if (data === startConnect) 
-            {
-                    client.id = getId();
-               
-                    console.log(client.id + "  connected");
-                    client.write(serverOK);
+         if (!err) {
+            if (client.id === undefined && (data.toString() ===  startConnect)) {
+                client.id = getId() ;
+                Clients[client.id] = data.toString();                                               //тип клиента QA/FILES по его id
+
+                if (Clients[client.id] === startConnect) {
+    
+                        createDir("E://PSCP//lr4//files//" + client.id.toString());
+                        filesChanks[client.id] = [];
+              
+                }
+                  console.log('Client ' + client.id + " connected: " + Clients[client.id]);
+                client.write(serverOK);
             }
-        }
+        } 
          else 
          {
             client.write(serverNO);
@@ -41,6 +55,10 @@ function getId()
     return ++counter;
 }
 
+function createDir(path) {
+    if (!fs.existsSync(path))
+        fs.mkdirSync(path);
+}
 
 server.listen(port, () => {
   console.log("Server listening on localhost: " + port);
