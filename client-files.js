c66a6a2539a3975e0e304305372b9c030338f187
Eugene Kurlovich <zhenikpggkurlovich@gmail.com>
2017-10-17 11:46:54 +0300

    last

--- client-files.js
+++ client-files.js
@@ -10,59 +10,58 @@ let directories = process.argv.slice(2);
 let files = []; 
 
 function ReadFilesInDirectory(dirPath) { 
-fs.readdir(path.normalize(dirPath), function (err, filess) { 
-filess.forEach(function (item) { 
-fs.stat(path.normalize(dirPath + path.sep + item), function (err, stat) { 
-if (stat.isDirectory()) { 
-ReadFilesInDirectory(dirPath + path.sep + item); 
-} 
-else if (stat.isFile()) { 
-files.push(dirPath + path.sep + item); 
-} 
-}) 
-}); 
-}); 
+    fs.readdir(path.normalize(dirPath), function (err, filess) {
+        filess.forEach(function (item) {
+            fs.stat(path.normalize(dirPath + path.sep + item), function (err, stat) {
+                if (stat.isDirectory()) {
+                    ReadFilesInDirectory(dirPath + path.sep + item);
+                }
+                else if (stat.isFile()) {
+                    files.push(dirPath + path.sep + item);
+                }
+            })
+        });
+    });
 } 
 
 directories.forEach((value) => { 
-ReadFilesInDirectory(value); 
+    ReadFilesInDirectory(value);
 }); 
 
 const client = new net.Socket(); 
 
 function sendFile() { 
-let filePath = files.pop(); 
-client.setNoDelay(true); 
-fs.readFile(filePath, function (err, data) { 
-client.write(data.toString('base64') + '{', function () { 
-client.write(path.basename(filePath) + '{', function () { 
-client.write("FEND" + '{', function () { }); 
-}); 
-}); 
-}); 
-
+    let filePath = files.pop();
+    client.setNoDelay(true);
+    fs.readFile(filePath, function (err, data) {
+        client.write(data.toString('base64') + '{', function () {
+            client.write(path.basename(filePath) + '{', function () {
+                client.write("FEND" + '{', function () { });
+            });
+        });
+    });
 } 
 
 client.setEncoding('utf8'); 
 
 client.connect(port, function () { 
-console.log('Connected'); 
-client.write(FILES); 
+    console.log('Connected');
+    client.write(FILES);
 }); 
 
 client.on('data', function (data) { 
-if (data === ASK && !isFILES) { 
-isFILES = true; 
-sendFile(); 
+    if (data === ASK && !isFILES) {
+        isFILES = true;
+        sendFile();
 } 
-if (files.length === 0) { 
-client.destroy(); 
-} 
-else if (data === "File saved" && files.length !== 0 && isFILES) { 
-sendFile(); 
-} 
+    if (files.length === 0) {
+        client.destroy();
+    }
+    else if (data === "File saved" && files.length !== 0 && isFILES) {
+        sendFile();
+    }
 }); 
 
 client.on('close', function () { 
-console.log('Connection closed'); 
+    console.log('Connection closed');
 });
--- server.js
+++ server.js
@@ -1,68 +1,64 @@
 const net = require('net'); 
 const path = require('path'); 
 const port = 8124; 
-let seed = 0; 
-const QA = 'QA'; 
-const DEC = 'DEC'; 
-const ASK = 'ASK'; 
-const FILES = 'FILES'; 
+let seed = 0;
+const QA = 'QA';
+const DEC = 'DEC';
+const ASK = 'ASK';
+const FILES = 'FILES';
 let fs = require('fs'); 
-let files = []; 
-let indexOfFile = 0; 
+let files = [];
+let indexOfFile = 0;
+let savedir = './save'
+const server = net.createServer((client) => {
+    let File;
+    let isQA = false;
+    let isFILES = false;
 
-const server = net.createServer((client) => { 
-let File; 
-let isQA = false; 
-let isFILES = false; 
+    client.id = Date.now() + ++seed;
+    client.setEncoding('utf8');
 
-client.id = Date.now() + ++seed; 
-client.setEncoding('utf8'); 
+    console.log('Client connected. Client ID: ' + client.id);
 
-console.log('Client connected. Client ID: ' + client.id); 
+    client.on('data', (data) => {
+        if (data === QA) {
+            isQA = true;
+            client.write(ASK);
+        }
+        if (data === FILES) {
+            files[client.id] = [];
+            fs.mkdir(savedir + path.sep + client.id,
+            function (err) {
+                if (err) {
+                console.log(err);
+                }
+            });
+            isFILES = true;
+            client.write(ASK);
+        }
+        else if (data === DEC) {
+        client.write(DEC);
+        client.destroy();
+        console.log('Client' + client.id + ' disconnected');
+        }
 
-client.on('data', (data) => { 
-if (data === QA) { 
-isQA = true; 
-client.write(ASK); 
-} 
-if (data === FILES) { 
-files[client.id] = []; 
-fs.mkdir("E:"+ path.sep + "qwe" + path.sep + client.id, 
-function (err) { 
-if (err) { 
-console.log(err); 
-} 
-} 
-); 
-isFILES = true; 
-client.write(ASK); 
-} 
-else if (data === DEC) { 
-client.write(DEC); 
-client.destroy(); 
-console.log('Client' + client.id + ' disconnected'); 
-} 
-
-else if (data !== FILES && data !== QA && isFILES) { 
-files[client.id].push(data.toString());
-
-if (data.toString().endsWith("FEND{")) { 
-let info = files[client.id].toString().split('{'); 
-info.pop(); 
-let buffer = Buffer.from(info[0], 'base64'); 
-fs.writeFile("E:"+ path.sep + "qwe" + path.sep + client.id + path.sep + client.id + "_" + info[1], buffer, 
-function () { 
-info = []; 
-files[client.id] = []; 
-client.write("File saved"); 
-}); 
-} 
-} 
-}); 
-
-client.on('end', () => console.log('Client disconnected')); 
+        else if (data !== FILES && data !== QA && isFILES) {
+            files[client.id].push(data.toString());
+            if (data.toString().endsWith("FEND{")) {
+                let info = files[client.id].toString().split('{');
+                info.pop();
+                let buffer = Buffer.from(info[0], 'base64');
+                fs.writeFile(savedir+ path.sep + client.id + path.sep + client.id + "_" + info[1], buffer, function () {
+                    info = [];
+                    files[client.id] = [];
+                    client.write("File saved");
+                });
+            }
+        }
+    });
+    client.on('end', () => console.log('Client disconnected'));
 }); 
 
 server.listen(port, () => { 
-console.log(`Server listening on localhost:${port}`); 
+    console.log(`Server listening on localhost:${port}`);
 }); 
